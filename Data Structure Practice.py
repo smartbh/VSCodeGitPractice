5a87f117d4fa6968dd79e8495afe79ce6c90074f
smartbh <smartbh@naver.com>
2022-10-11 18:44:22 +0900

    DataStructChange Practice

---
+++ Data Structure Practice.py
@@ -0,0 +1,13 @@
+#자료구조의 변경
+# 여러가지 자료형 변환 시켜보기
+menu = {"커피", "우유", "주스"}
+print(menu, type(menu))
+
+menu = list(menu)
+print(menu, type(menu))
+
+menu = tuple(menu)
+print(menu, type(menu))
+
+menu = set(menu)
+print(menu, type(menu))
