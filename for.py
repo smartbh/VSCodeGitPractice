4d764e7b9d48084b3920c6a8fc074295bbb5bcdf
smartbh <smartbh@naver.com>
2022-10-12 19:13:23 +0900

    for

---
+++ for.py
@@ -0,0 +1,10 @@
+#print("대기번호 : 1")
+
+
+for waiting_no in range(1,6):
+    print("대기번호 : {0}".format(waiting_no))
+
+starbucks = ["아이언맨","토르","헐크"]
+
+for customer in starbucks:
+    print("{0}님, 커피가 준비됐습니다.".format(customer))
