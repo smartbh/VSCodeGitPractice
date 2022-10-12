446b76145a5cbadbeea49f04bdc099136906bb9b
smartbh <smartbh@naver.com>
2022-10-12 18:41:41 +0900

    if state

--- ifState.py
+++ ifState.py
@@ -7,3 +7,15 @@ elif weather == "미세먼지":
     print("마스크써라")
 else :
     print("집에있어")
+
+
+temp = int(input("기온을 입력하세요 : "))
+if 30 <= temp:
+    print("존나덥다 벗고나가라")
+elif 10 <= temp and temp < 30:
+    print("놀기 좋은 날씨다")
+elif 0 <= temp < 10:
+    print("춥다 챙겨입어라")
+else:
+    print("얼어 뒤지니깐 나가지마라")
+    
