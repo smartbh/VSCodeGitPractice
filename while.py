8a4b9949c8b335f403159f0febfc68c1f13328af
smartbh <smartbh@naver.com>
2022-10-12 20:18:43 +0900

    while

--- while.py
+++ while.py
@@ -14,3 +14,16 @@ person = "unknown"
 while person != customer:
     print("{0}, 커피가 준비됐습니다.".format(customer))
     person = input("이름이 어떻게 되세요? ")
+
+
+absent = [2,5] #결석
+
+no_book = [7]
+
+for student in range(1,11):
+    if student in absent:
+        continue
+    elif student in no_book:
+        print("오늘 수업은 여기까지 {0}는 교무실로 따라와".format(student))
+        break
+    print("{0}, 책을 읽어봐".format(student))
