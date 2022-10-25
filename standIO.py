99a41d068b6e262e8ba094c98bf52a81d6b2215d
smartbh <smartbh@naver.com>
2022-10-25 16:14:39 +0900

    output format

---
+++ standIO.py
@@ -0,0 +1,28 @@
+import sys
+
+
+import sys
+
+# print("Python", "Java", sep=" . ", end="?") #sep과 end
+# #end의 디폴트는 줄바꿈
+# print("무엇이 더 재밌을까요?")
+
+# print("Python", "Java", file=sys.stdout)
+# print("Python", "Java", file=sys.stderr)
+
+#출력 포맷 바꿔보기
+
+#시험 성적
+# scores = {"수학":0, "영어":50,"코딩":100}
+
+# for subject, score in scores.items():
+#     #print(subject,score)
+#     print(subject.ljust(8),str(score).rjust(4),sep=":") #문자열에 있는 메소드들
+
+
+#은행 대기순번표
+# for num in range(1,21):
+#     print("대기번호 : " + str(num).zfill(3))
+
+answer = input("아무 값이나 입력하세요:")
+print(type(answer))
